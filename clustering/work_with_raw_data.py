548ab9290f4403867b625ae3fe1e75f864a9557e
jamilyaerk <erkenovaj@gmail.com>
2023-03-22 21:29:14 +0300

    Producing data for clustering

--- clustering/work_with_raw_data.py
+++ clustering/work_with_raw_data.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+import numpy as np
 
 tracks = []
 track_id = 0
-with open("event.txt", "r") as f:
+with open("../data/event.txt", "r") as f:
     for i in f:
         temp = []
         tracks.append([])
@@ -55,26 +56,57 @@ print(len(tracks_df['theta'].unique()))
 #print(tracks[2])
 #-----------------------------------------------
 
-tracks_df.to_csv('Tracks.csv')
+un_tr = tracks_df['track_number'].unique().tolist()
 
+tracks_df.to_csv('../data/Tracks.csv')
+
+indice = []
+for i in un_tr:
+    if len(tracks_df[tracks_df['track_number'] == i]['theta'].unique()) > 1:
+        indice.append(i)
+
+len(indice)
+
+#different thetas in the same tracks
+tracks_df[tracks_df['track_number'].isin(indice)]
+tracks_df[tracks_df['track_number'] == 65]
+
+
+#------------------r*phi------------------------
+tracks_df['rphi'] = np.sign(tracks_df['y']) * np.sign(tracks_df['x']) *  np.sqrt(tracks_df['x']**2 * tracks_df['y']**2)  * tracks_df['phi'] 
+
+tracks_df[tracks_df['hit-index'] == 21806.0]
+
+tracks_df[tracks_df['track_number'] == 1269]
+
+tracks_df['r'] = np.sign(tracks_df['y']) * np.sqrt(tracks_df['x']**2 * tracks_df['y']**2) 
+
+tracks_df[['track_number','rphi', 'z', 'r', 'phi']].to_csv('check_line.csv')
+
+#plt.scatter(tracks_df[tracks_df['track_number'] == 2]['rphi'], tracks_df[tracks_df['track_number'] == 2]['z']) 
+#tracks_df[tracks_df['track_number'] == 3][['track_number','r', 'phi', 'z', 'x', 'y']]
+
+
+#monotonous check
 
 #--------------------REGRESSION---------------------
 
 r_sq = []
 coef = []
 length = []
-un_tr = tracks_df['track_number'].unique().tolist()
+
+tracks_df['constant'] = 1
 
 for i in un_tr:
     if len(tracks_df[tracks_df['track_number'] == i]) > 1:
-        x = tracks_df[tracks_df['track_number'] == i][['x', 'z']]
-        y = tracks_df[tracks_df['track_number'] == i]['y']
+        x = tracks_df[tracks_df['track_number'] == i][['constant', 'rphi']]
+        y = tracks_df[tracks_df['track_number'] == i]['z']
         model = LinearRegression().fit(x, y)
         r_sq.append(model.score(x, y))
         coef.append(model.coef_)
     else: 
         r_sq.append(None)
-        coef.append(None)
+        coef.append(model.coef_)
     length.append(len(tracks_df[tracks_df['track_number'] == i]))
 
 
@@ -83,6 +115,8 @@ regress['r_squared'] = r_sq
 regress['coef_x_z'] = coef
 regress['length'] = length
 
-regress[regress['r_squared'] > 0.90]
+regress.to_csv('data_for_cluster.csv')
+
+regress[regress['r_squared'] > 0.99]
 
 regress.head(5)
