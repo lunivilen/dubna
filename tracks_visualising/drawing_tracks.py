672fcf0a654fb8362e673a88801874e19ee058c7
lunivilen <ilya24624864@gmail.com>
2023-03-30 12:31:01 +0300

    Сортировка точек внутри трека

--- tracks_visualising/drawing_tracks.py
+++ tracks_visualising/drawing_tracks.py
@@ -3,19 +3,20 @@ import PyQt6.QtWidgets as pqg
 from PyQt6.QtGui import QColor
 from PyQt6.QtCore import Qt
 import numpy as np
+import pandas as pd
 
 
 def merge_tracks(track_one, track_two):
     for point in track_two:
         if point not in track_one:
             track_one.append(point)
-    return sorted(track_one)
+    return track_one
 
 
 # Breaking tracks from a file into separate hits
 tracks = []
 track_id = 0
-with open("event1.txt", "r") as f:
+with open("event0.txt", "r") as f:
     for i in f:
         temp = []
         tracks.append([])
@@ -23,8 +24,8 @@ with open("event1.txt", "r") as f:
         amount_characteristics = 0
         j = 0
         while j < len(mas):
-            if amount_characteristics != 9:
-                if amount_characteristics != 0 and len(temp) < 3:
+            if amount_characteristics != 10:
+                if amount_characteristics != 0 and amount_characteristics != 1 and len(temp) < 3:
                     temp.append(float(mas[j]))
                 amount_characteristics += 1
                 j += 1
@@ -38,18 +39,41 @@ with open("event1.txt", "r") as f:
             amount_characteristics = 0
         track_id += 1
 
+# Merge same tracks
+similarity_factor = 0.4
 i = 0
 while i < len(tracks):
     j = 0
     while j < len(tracks):
         if tracks[j][0] in tracks[i] and i != j:
             temp_mas = tracks[i] + tracks[j]
-            if len(temp_mas) / len(np.unique(temp_mas)) > 0.8:
+            if len(temp_mas) / len(np.unique(temp_mas)) > similarity_factor:
                 tracks[i] = merge_tracks(tracks[i], tracks[j])
-            tracks.pop(j)
-        else:
-            j += 1
+                tracks.pop(j)
+                j -= 1
+        j += 1
     i += 1
+
+# Sort the hits in the correct order after merge
+for i in range(len(tracks)):
+    df = pd.DataFrame(tracks[i], columns=[0, 1, 2])
+    scatter_x = df[0].max() - df[0].min()
+    scatter_y = df[1].max() - df[1].min()
+    scatter_z = df[2].max() - df[2].min()
+    if max(scatter_x, scatter_y, scatter_z) == scatter_x:
+        tracks[i] = list(df.sort_values(0, ascending=True).values)
+    elif max(scatter_x, scatter_y, scatter_z) == scatter_y:
+        tracks[i] = list(df.sort_values(1, ascending=True).values)
+    else:
+        tracks[i] = list(df.sort_values(2, ascending=True).values)
+    tracks[i] = list(map(list, tracks[i]))
+
+with open("new_event.txt", "w") as f:
+    for i in tracks:
+        for j in i:
+            for k in j:
+                f.write(str(k) + ", ")
+        f.write("\n")
 # Discard all characteristics of hits, except for coordinates
 tracks_new = []
 indexes = []
@@ -89,7 +113,7 @@ graphs = gl.GLGraphItem()
 graphs.setData(nodePositions=np.array(tracks_new),
                edges=np.array(indexes),
                edgeColor=QColor(Qt.GlobalColor.green),
-               nodeColor=QColor(Qt.GlobalColor.white),
+               nodeColor=QColor(Qt.GlobalColor.gray),
                edgeWidth=2)
 plot.addItem(graphs)
 
