205f97ab441a019be13413983608584a3b065b3e
lunivilen <ilya24624864@gmail.com>
2023-04-26 15:18:07 +0300

    Speed up merging

--- result/merging.py
+++ result/merging.py
@@ -18,7 +18,7 @@ def get_vector_length(vec):
     return math.sqrt(vec[0] ** 2 + vec[1] ** 2 + vec[2] ** 2)
 
 
-def check_direction(track_one, track_two):
+def co_directed(track_one, track_two):
     normal = get_vector(track_one[0], track_two[-1])
     reverse = get_vector(track_one[0], track_two[0])
     return get_vector_length(reverse) > get_vector_length(normal)
@@ -36,7 +36,7 @@ def distance_to_line(m0, m1, m_check):
     return d
 
 
-def merging(tracks: list, allowable_angle=160, allowable_error=700, allowable_length=700, allowable_distance=35):
+def merging(tracks: list, allowable_angle=160, allowable_length=700, allowable_distance=35):
     start = time()
     print("Staring real merging")
     i = 0
@@ -52,39 +52,43 @@ def merging(tracks: list, allowable_angle=160, allowable_error=700, allowable_le
             vec_1 = average_vec([tracks[i][k][1:4] for k in range(3)])
 
         while j < len(tracks) and i < len(tracks):
+            # Check length of tracks
             if len(tracks[j]) < 2 or len(tracks[i]) < 2 or i == j:
                 j += 1
                 continue
 
-            # Correcting the direction of the track
-            end = -1 if check_direction(tracks[i], tracks[j]) else 0
+            # Check distance between tracks
+            vec_a = get_vector(tracks[i][0][1:4], tracks[j][-1][1:4])
+            if get_vector_length(vec_a) > allowable_length:
+                j += 1
+                continue
+
+            # Discarding not co-directed tracks
+            if not co_directed(tracks[i], tracks[j]):
+                j += 1
+                continue
 
             # Get vector of the second track
             if len(tracks[j]) == 2:
-                vec_2 = get_vector(tracks[j][end][1:4], tracks[j][end - 1 if end < 0 else end + 1][1:4])
+                vec_2 = get_vector(tracks[j][-1][1:4], tracks[j][-2][1:4])
             else:
-                vec_2 = average_vec([tracks[j][end][1:4],
-                                     tracks[j][end - 1 if end < 0 else end + 1][1:4],
-                                     tracks[j][end - 2 if end < 0 else end + 2][1:4]])
+                vec_2 = average_vec([tracks[j][-k][1:4] for k in range(1, 4)])
+
+            # Check angele between tracks
             if angle_between_vec(vec_1, vec_2) < allowable_angle:
                 j += 1
                 continue
 
-            # We build a triangle from the last point of the second vector
-            # and the last two points of the first vector
-            vec_a = get_vector(tracks[i][0][1:4], tracks[j][end][1:4])
-            if get_vector_length(vec_a) < allowable_length:
-                distance = distance_to_line(np.array(tracks[i][0][1:4]),
-                                            np.array(tracks[i][1][1:4]),
-                                            np.array(tracks[j][end][1:4]))
-                if distance < allowable_distance:
-                    tracks[i].extend(tracks[j])
-                    tracks[i] = sort_hits(tracks[i])
-                    tracks.pop(j)
-                    i -= 1
-                    break
-                else:
-                    j += 1
+            # Check distance between straight lines formed by tracks
+            distance = distance_to_line(np.array(tracks[i][0][1:4]),
+                                        np.array(tracks[i][1][1:4]),
+                                        np.array(tracks[j][-1][1:4]))
+            if distance < allowable_distance:
+                tracks[i].extend(tracks[j])
+                tracks[i] = sort_hits(tracks[i])
+                tracks.pop(j)
+                i -= 1
+                break
             else:
                 j += 1
         i += 1
