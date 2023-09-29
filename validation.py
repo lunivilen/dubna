39517a08db62080953b34ff1d70ad57317f81ffa
jamilyaerk <HHfwbd7wny>
2023-09-29 14:42:41 +0300

    Validation fix

--- validation.py
+++ validation.py
@@ -24,6 +24,7 @@ def get_characteristics(tracks, hits, n, ratio):
     reco_tracks = set()
     fake_tracks = set()
     duplicate_tracks = []
+    reco_dupl_tracks = []
 
     # Replace hits in track with id of their real track
     tracks_hits = replace_hits_to_track_id(tracks, hits)
@@ -34,22 +35,25 @@ def get_characteristics(tracks, hits, n, ratio):
         # Find the most common real track id in reco track
         reco_track_id = max(tracks_hits[i], key=tracks_hits[i].count)
 
+        if tracks_hits[i].count(reco_track_id) / len(tracks_hits[i]) >= ratio:
+            reco_dupl_tracks.add(reco_track_id)
+
         # Check duplicates
         if reco_track_id in reco_tracks:
             duplicate_tracks.append(reco_track_id)
             continue
 
-        # Check ration and mark track as reco or fake
+        # Check ratio and mark track as reco or fake
         if tracks_hits[i].count(reco_track_id) / len(tracks_hits[i]) >= ratio:
             reco_tracks.add(reco_track_id)
         else:
             fake_tracks.add(i)
-    return reco_tracks, fake_tracks, duplicate_tracks
+    return reco_tracks, fake_tracks, duplicate_tracks, reco_dupl_tracks
 
 
 def calc_characteristics(tracks, hit_list, track_dict, secondary_track_list=None, min_length=9, ratio=0.5):
     # Get all lists of necessary data
-    reco_track_list, fake_track_list, duplicate_track_list = get_characteristics(tracks, hit_list, min_length, ratio)
+    reco_track_list, fake_track_list, duplicate_track_list, reco_dupl_tracks = get_characteristics(tracks, hit_list, min_length, ratio)
     real_track_list = get_real_tracks(track_dict, min_length)
 
     # Remove secondary track id from data if in necessary
@@ -68,9 +72,9 @@ def calc_characteristics(tracks, hit_list, track_dict, secondary_track_list=None
 
     characteristic_dict = {
         "efficiency": len(reco_track_list) / num_real_track if num_real_track else 0,
-        "fake_rate": len(fake_track_list) / num_real_track if num_real_track else 0,
+        "fake_rate": len(fake_track_list) / num_proto_track if num_proto_track else 0,
         "duplication_rate": len(duplicate_track_list) / num_proto_track if num_real_track else 0,
-        "purity": len(reco_track_list) / num_proto_track if num_proto_track else 0
+        "purity": len(reco_dupl_tracks) / num_proto_track if num_proto_track else 0
     }
     return characteristic_dict
 
